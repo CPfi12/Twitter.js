93553fd4e1d5366c38925d739a2e9313d325f9ab
Claire Pfister <clairepfis@gmail.com>
2017-04-25 10:35:18 -0400

    fixed

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require( 'express' );
 const app = express();
 
 app.use('*',function(req,res,next){
-	//console.log(req);
-	console.log(req.method+" "+req.originalUrl+" "+res.statusCode);
+	res.on("finish", function(){
+  		console.log(req.method+" "+req.originalUrl+" "+res.statusCode);
+  	});
 	next();
-})
+});
+
+app.use('/special/*',function(req,res,next){
+	console.log("you reached the special area.");
+	console.log('edit! 2');
+	next();
+});
+
 app.get('/',function(req,res){
 	res.send('Welcome!');
 });
